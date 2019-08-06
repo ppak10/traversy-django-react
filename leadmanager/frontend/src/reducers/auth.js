4f5a52539c1e1157303d2aa2f7c87a0d2e21feb7
Peter <ppak10@gmail.com>
2019-08-06 11:21:23 -0700

    Video 7 - Front-end Authentication
    Adds functionality for user login and logout allows for user to access private routes on the backend

--- leadmanager/frontend/src/reducers/auth.js
+++ leadmanager/frontend/src/reducers/auth.js
@@ -1,4 +1,4 @@
-import {USER_LOADING, USER_LOADED, AUTH_ERROR} from '../actions/types';
+import {USER_LOADING, USER_LOADED, AUTH_ERROR, LOGIN_FAIL, LOGIN_SUCCESS, LOGOUT_SUCCESS, REGISTER_SUCCESS, REGISTER_FAIL} from '../actions/types';
 
 const initialState = {
     token: localStorage.getItem('token'),
@@ -20,8 +20,20 @@ export default function(state = initialState, action) {
                 isAuthenticated: true,
                 isLoading: false,
                 user: action.payload
+            };
+        case LOGIN_SUCCESS:
+        case REGISTER_SUCCESS:
+            localStorage.setItem('token', action.payload.token);
+            return {
+                ...state,
+                ...action.payload,
+                isAuthenticated: true,
+                isLoading: false,
             }
         case AUTH_ERROR:
+        case LOGIN_FAIL:
+        case LOGOUT_SUCCESS:
+        case REGISTER_FAIL:
             localStorage.removeItem('token');
             return {
                 ...state,
