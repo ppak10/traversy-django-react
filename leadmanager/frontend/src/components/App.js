8d7913b180b1fb8f00ae27f5f9aa3f4806b8069d
Peter <ppak10@gmail.com>
2019-08-05 17:48:27 -0700

    Video 2 - Adds React to Django
    Adds the basic framework for react to the Django app

---
+++ leadmanager/frontend/src/components/App.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+
+import Header from './layout/Header';
+import Dashboard from './leads/Dashboard';
+
+class App extends Component {
+    render () {
+        return (
+            <>
+                <Header />
+                <div className="container">
+                    <Dashboard />
+                </div>
+            </>
+        )
+    }
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: [
+                            "@babel/preset-env",
+                            "@babel/preset-react"
+                        ],
+                        plugins: [
+                            "transform-class-properties"
+                        ]
+                    }
+                }
+            }
+        ]
+    }
+}
