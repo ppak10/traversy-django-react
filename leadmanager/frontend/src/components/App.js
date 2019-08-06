10f415c63fc9496cdffaef263c637cc79499af34
Peter <ppak10@gmail.com>
2019-08-05 22:44:39 -0700

    Video 6 - Adds Frontend Private Routes
    Adds front-end private routes and framework to allows for token authentication from the front-end to the back-end

--- leadmanager/frontend/src/components/App.js
+++ leadmanager/frontend/src/components/App.js
@@ -3,12 +3,17 @@ import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
 import {Provider as AlertProvider} from 'react-alert';
 import AlertTemplate from 'react-alert-template-basic';
+import {HashRouter as Router, Route, Switch, Redirect} from 'react-router-dom';
 
 import store from '../store';
 
 import Header from './layout/Header';
 import Dashboard from './leads/Dashboard';
 import Alerts from './layout/Alerts';
+import Login from './accounts/Login';
+import Register from './accounts/Register';
+import PrivateRoute from './common/PrivateRoute';
+import {loadUser} from '../actions/auth';
 
 // Alert Options
 const alertOptions = {
@@ -17,17 +22,26 @@ const alertOptions = {
 };
 
 class App extends Component {
+    componentDidMount() {
+        store.dispatch(loadUser())
+    }
     render () {
         return (
             <Provider store={store}>
                 <AlertProvider template={AlertTemplate} {...alertOptions}>
-                    <>
-                        <Header />
-                        <Alerts />
-                        <div className="container">
-                            <Dashboard />
-                        </div>
-                    </>
+                    <Router>
+                        <>
+                            <Header />
+                            <Alerts />
+                            <div className="container">
+                                <Switch>
+                                    <PrivateRoute exact path="/" component={Dashboard} />
+                                    <Route exact path="/register" component={Register} />
+                                    <Route exact path="/login" component={Login} />
+                                </Switch>
+                            </div>
+                        </>
+                    </Router>
                 </AlertProvider>
             </Provider>
         )
